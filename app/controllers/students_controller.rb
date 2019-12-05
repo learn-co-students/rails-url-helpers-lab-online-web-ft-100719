c7268f6ca9d95542ad0c3104ccd71d246c076048
Alyssa Yates <bosslys@icloud.com>
2019-12-05 23:26:12 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    set_student
+    if !@student.active
+      @student.active = true
+    else
+      @student.active = false
+    end
+
+    if @student.save
+      redirect_to @student
+    end
   end
 
   private
