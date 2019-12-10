047fad3452ee37d7792de6de5ef920f71bb2ec3f
figueroar <raul.a.figueroa@hotmail.com>
2019-12-09 19:33:36 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,10 +6,27 @@ class StudentsController < ApplicationController
   end
 
   def show
+    #binding.pry
+    @student = Student.find(params[:id])
+  end
+
+  def update 
+    #binding.pry
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.update(active: params[:active])
+      @student.active = true
+      @student.save
+      redirect_to "/students/#{@student.id}"
+    elsif @student.active == true
+      @student.update(active: params[:active])
+      @student.active = false
+      @student.save
+      redirect_to "/students/#{@student.id}"
+    end
   end
 
   private
-
     def set_student
       @student = Student.find(params[:id])
     end
