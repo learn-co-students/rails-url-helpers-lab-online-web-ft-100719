341dda58fd27b214c3b9d196bd335bc2d3fc3f69
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-12-12 15:04:35 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,16 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    if @student.active== false
+      @status= 'inactive'
+    elsif @student.active == true
+      @status= 'active'
+    end 
   end
 
   private
